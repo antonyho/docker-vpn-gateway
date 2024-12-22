71d0375e779c011ab1e34686287e7136bcb3b453
Antony Ho <ntonyworkshop@gmail.com>
2024-12-22 01:39:45 +0100

    Fix the issue where the container terminates after completing the entrypoint script

--- nordvpn-client/entrypoint.sh
+++ nordvpn-client/entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 nordvpn login --token ${NORDVPN_TOKEN}
 nordvpn set technology ${TECHNOLOGY}
 nordvpn set killswitch on
 nordvpn set autoconnect on
 nordvpn connect
+
+tail -f /dev/null # Keep the container running
