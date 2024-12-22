0f6bb060b6415c0160dffcec29322485d2352865
Antony Ho <ntonyworkshop@gmail.com>
2024-12-22 12:49:45 +0100

    Fix the startup process

--- nordvpn-client/entrypoint.sh
+++ nordvpn-client/entrypoint.sh
@@ -3,11 +3,14 @@
 set -e
 
 echo "Starting NordVPN daemon..."
-/etc/init.d/nordvpn start &
+/etc/init.d/nordvpn start
+
+# Give the daemon a bit of time to spin up
+sleep 5
 
 echo "Logging in NordVPN..."
 
-nordvpn login --token ${NORDVPN_TOKEN}
+nordvpn login --token ${NORDVPN_TOKEN} 2>/dev/null || true
 nordvpn set technology ${TECHNOLOGY}
 nordvpn set killswitch on
 nordvpn set autoconnect on
